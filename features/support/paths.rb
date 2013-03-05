877b15fda184f2388aecfee72461612ca9550d59
Harpreet Singh <harpreet-singh-1@uiowa.edu>
2013-03-04 19:25:29 -0800

    Update paths.rb

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -16,7 +16,7 @@ module NavigationHelpers
     when /^the (RottenPotatoes )?home\s?page$/ 
     then '/'
     when /^the movies page$/ 
-    then '/movies'
+    then '/movies/'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
